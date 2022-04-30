52a99daa6b69f8712d005583d0b1de7e442f9922
phil_tan <67247902+petaleaf@users.noreply.github.com>
2022-04-30 17:46:38 +0800

    Add files via upload

---
+++ extract_id_from_fasta.py
@@ -0,0 +1,23 @@
+# @Time     :  16:00
+# @Author   : 谈佳瑞
+# @FileName : extract_id_from_fasta.py
+# @Software : Pycharm
+
+
+from Bio import SeqIO
+import sys
+
+
+if len(sys.argv) != 3:
+    print('Usage: python extract_id_from_fasta.py <fasta.fasta> <xx.out >')
+    sys.exit()
+idlist = []
+for seqrecord in SeqIO.parse(sys.argv[1],"fasta"):
+    idlist.append(seqrecord.id+"\n")
+print(len(idlist))
+
+
+with open(sys.argv[2],"w",encoding="UTF-8") as outputfile:
+    for id in idlist:
+        outputfile.write(id)
+
